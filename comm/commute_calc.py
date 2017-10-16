223ee491744f963041449594b0da34ee3c84d68d
barretobrock <barretobrock2@gmail.com>
2017-10-16 18:43:23 -0500

    B-1: Test new import method

---
+++ comm/commute_calc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import inspect
+import os
+import sys
+import socket
+# set system paths for importing custom modules/functions
+cur_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+hostname = socket.gethostname()
+if '__file__' in globals():
+    # if not running in debugger, assign parent directory of file to system paths
+    parent_dir = os.path.dirname(os.path.dirname(cur_dir))
+    print(parent_dir)
+    sys.path.insert(0, os.path.join(parent_dir, 'Scripts'))
+else:
+    # otherwise set system paths based on project directory in PyCharm
+    sys.path.insert(0, cur_dir)
