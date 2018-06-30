62143cb73c4728b650677bf63f3a660622a1cf6c
barretobrock <barret.obrock@gmail.com>
2018-06-30 11:31:47 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='blue2',
+    version='0.1',
+    packages=['', 'agg', 'comm', 'setup', 'camera', 'logger', 'primary', 'sensors', 'signals', 'weather', 'switches'],
+    url='',
+    license='',
+    author='bobrock',
+    author_email='',
+    description=''
+)
